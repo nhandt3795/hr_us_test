7f4b38282a6418a2feba56a993e9e896c7aa748f
nhandtlqa <nhandt@lqa.com.vn>
2023-07-04 09:47:49 +0700

    Update tag

--- tests/feature_1/functionC.spec.js
+++ tests/feature_1/functionC.spec.js
@@ -1,11 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('@FunctionC Function C Tests', async () => {
-    test('@C01 Testcase 1', async ({}) => {
+    test('@C01 Testcase 1', async ({baseURL}) => {
         console.log("[Test] Function C -------- Testcase 1")
+        console.log(baseURL)
     });
     
-    test('@C02 Testcase 2', async () => {
+    test('@C02 Testcase 2', async ({baseURL}) => {
         console.log("[Test] Function C -------- Testcase 2")
+        console.log(baseURL)
     });
 })
--- tests/feature_1/functionD.spec.js
+++ tests/feature_1/functionD.spec.js
@@ -1,11 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('@FunctionD Function D Tests', async () => {
-    test('@D01 Testcase 1', async ({}) => {
+    test('@D01 Testcase 1', async ({baseURL}) => {
         console.log("[Test] Function D -------- Testcase 1")
+        console.log(baseURL)
     });
     
-    test('@D02 Testcase 2', async () => {
+    test('@D02 Testcase 2', async ({baseURL}) => {
         console.log("[Test] Function D -------- Testcase 2")
+        console.log(baseURL)
     });
 })
